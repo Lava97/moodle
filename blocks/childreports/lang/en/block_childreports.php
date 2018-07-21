d5531735a31559ad0d00e9ac1e5cfd04c1ca7cdf
Lalit Chandwani <lalitdc97@gmail.com>
2018-07-21 22:50:09 +0530

    Create block_childreports.php

---
+++ blocks/childreports/lang/en/block_childreports.php
@@ -0,0 +1,31 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_childreports', language 'en'
+ *
+ * @package   block_childreports
+ * @copyright 2018 Lalit Chandwani
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configtitleleaveblanktohide'] = 'Block title (no title if blank)';
+$string['newchildreportsblock'] = '(new Child Reports block)';
+$string['pluginname'] = 'Child Reports';
+$string['childreports'] = 'Child Reports';
+$string['childreports:addinstance'] = 'Add a new child reports block';
+$string['childreports:myaddinstance'] = 'Add a new child reports block to the My Moodle page';
