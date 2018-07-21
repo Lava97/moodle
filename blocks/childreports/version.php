0a1fb1425aa2a7f0a8a7ea19394f5965603201cc
Lalit Chandwani <lalitdc97@gmail.com>
2018-07-21 22:45:32 +0530

    Create version.php

---
+++ blocks/childreports/version.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * @package    block_childreports
+ * @copyright  2018 Lalit Chandwani
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$plugin->component = 'block_childreports';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
+$plugin->version = 2018071900;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2015111600; // YYYYMMDDHH (This is the release version for Moodle 3.0.2
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = 'v3.0-r1'; // This is the first revision for Moodle 3.0.x branch.
